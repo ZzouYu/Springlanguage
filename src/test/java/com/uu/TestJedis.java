2ad87246c634137ef3034ac6edeaaea2d3f73ffd
ZzouYu <874881417@qq.com>
2018-06-06 14:02:54 +0800

    1

--- src/test/java/com/uu/TestJedis.java
+++ src/test/java/com/uu/TestJedis.java
@@ -13,7 +13,7 @@ public class TestJedis {
           System.out.println("连接成功");
           //设置 redis 字符串数据
           jedis.set("runoobkey", "www.runoob.com");
-          // 获取存储的数据并输出
+          // 获取存储的数据并输出 
           System.out.println("redis 存储的字符串为: "+ jedis.get("runoobkey"));
 	}
 }
